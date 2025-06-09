1d1f0109b5593ff834366d51df58f4ec94523871
jamlmao <jajamagustin0624@gmail.com>
2025-06-09 16:38:47 +0800

    feat change the color scheme again and need to edit the data.js

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import HomeSearch from "@/components/HomeSearch";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <section className="relative py-16 md:py-28 dotted-background">
         <div className="max-w-4xl mx-auto text-center">
            <div className="mb-8">
-           <h1 className="text-2xl md:text-4xl font-bold mb-4 bg-gradient-to-r from-[#c11e38] to-[#220b34] text-transparent bg-clip-text tracking-tighter pr-2 pb-2">
+           <h1 className="text-2xl md:text-6xl font-bold mb-4 bg-gradient-to-r from-[#C62828] to-[#B0BEC5] text-transparent bg-clip-text tracking-tighter pr-2 pb-2">
                 Welcome to Rent N' Go  Drive Luxury, Live Prestige.
               </h1>
               <p className="text-xl text-gray-500 mb-8 max-w-2xl mx-auto">
@@ -17,7 +18,7 @@ export default function Home() {
            </div>
 
           {/* Search bar */}
-
+          <HomeSearch />            
         </div>
       </section>
 
